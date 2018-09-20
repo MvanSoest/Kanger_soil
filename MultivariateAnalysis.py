e0e54f99bf4e3ac7b5448509ee629f0d3975d2b0
MvanSoest <maud.vansoest@gmail.com>
2018-09-20 12:14:07 +0100

    Purpose of folder

---
+++ MultivariateAnalysis.py
@@ -0,0 +1,172 @@
+from pydoc import help  # can type in the python console `help(name of function)` to get the documentation
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from scipy import stats
+from IPython.display import display, HTML
+
+# figures inline in notebook
+%matplotlib inline
+
+np.set_printoptions(suppress=True)
+
+DISPLAY_MAX_ROWS = 20  # number of max rows to print for a DataFrame
+pd.set_option('display.max_rows', DISPLAY_MAX_ROWS)
+
+# %qtconsole # uncomment if wanting to use a python console aside a notebook
+
+# reading multivariate analysis data into python
+data = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header=None)
+data.columns = ["V"+str(i) for i in range(1, len(data.columns)+1)]  # rename column names to be similar to R naming convention
+data.V1 = data.V1.astype(str)
+X = data.loc[:, "V2":]  # independent variables data
+y = data.V1  # dependednt variable data
+data
+
+# Matrix scatterplot
+data.loc[:, "V2":"V6"] # if you only want to use part of the variables
+pd.tools.plotting.scatter_matrix(data.loc[:, "V2":"V6"], diagonal="kde") # kde for kernel density on the diagonal or hist for a histogram
+plt.tight_layout()
+plt.show()
+
+# scatter plot with data points labelled per group to examine features from above in detail
+sns.lmplot("V4", "V5", data, hue="V1", fit_reg=False);
+
+# profile plot to show variation within each variables
+ax = data[["V2","V3","V4","V5","V6"]].plot()
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5));
+
+# statistics
+X.apply(np.mean)
+X.apply(np.std)
+
+class2data = data[y=="2"] # to extract out data for just 1 cultivar
+class2data.loc[:, "V2":].apply(np.mean)
+
+def printMeanAndSdByGroup(variables, groupvariable):
+    data_groupby = variables.groupby(groupvariable)
+    print("## Means:")
+    display(data_groupby.apply(np.mean))
+    print("\n## Standard deviations:")
+    display(data_groupby.apply(np.std))
+    print("\n## Sample sizes:")
+    display(pd.DataFrame(data_groupby.apply(len)))
+printMeanAndSdByGroup(X, y)
+
+# between and within group variance
+def calcWithinGroupsVariance(variable, groupvariable):
+    # find out how many values the group variable can take
+    levels = sorted(set(groupvariable))
+    numlevels = len(levels)
+    # get the mean and standard deviation for each group:
+    numtotal = 0
+    denomtotal = 0
+    for leveli in levels:
+        levelidata = variable[groupvariable==leveli]
+        levelilength = len(levelidata)
+        # get the standard deviation for group i:
+        sdi = np.std(levelidata)
+        numi = (levelilength)*sdi**2
+        denomi = levelilength
+        numtotal = numtotal + numi
+        denomtotal = denomtotal + denomi
+    # calculate the within-groups variance
+    Vw = numtotal / (denomtotal - numlevels)
+    return Vw
+
+def calcBetweenGroupsVariance(variable, groupvariable):
+    # find out how many values the group variable can take
+    levels = sorted(set((groupvariable)))
+    numlevels = len(levels)
+    # calculate the overall grand mean:
+    grandmean = np.mean(variable)
+    # get the mean and standard deviation for each group:
+    numtotal = 0
+    denomtotal = 0
+    for leveli in levels:
+        levelidata = variable[groupvariable==leveli]
+        levelilength = len(levelidata)
+        # get the mean and standard deviation for group i:
+        meani = np.mean(levelidata)
+        sdi = np.std(levelidata)
+        numi = levelilength * ((meani - grandmean)**2)
+        denomi = levelilength
+        numtotal = numtotal + numi
+        denomtotal = denomtotal + denomi
+    # calculate the between-groups variance
+    Vb = numtotal / (numlevels - 1)
+    return(Vb)
+
+# between and within covariance
+def calcWithinGroupsCovariance(variable1, variable2, groupvariable):
+    levels = sorted(set(groupvariable))
+    numlevels = len(levels)
+    Covw = 0.0
+    # get the covariance of variable 1 and variable 2 for each group:
+    for leveli in levels:
+        levelidata1 = variable1[groupvariable==leveli]
+        levelidata2 = variable2[groupvariable==leveli]
+        mean1 = np.mean(levelidata1)
+        mean2 = np.mean(levelidata2)
+        levelilength = len(levelidata1)
+        # get the covariance for this group:
+        term1 = 0.0
+        for levelidata1j, levelidata2j in zip(levelidata1, levelidata2):
+            term1 += (levelidata1j - mean1)*(levelidata2j - mean2)
+        Cov_groupi = term1 # covariance for this group
+        Covw += Cov_groupi
+    totallength = len(variable1)
+    Covw /= totallength - numlevels
+    return Covw
+calcWithinGroupsCovariance(X.V8, X.V11, y)
+
+def calcBetweenGroupsCovariance(variable1, variable2, groupvariable):
+    # find out how many values the group variable can take
+    levels = sorted(set(groupvariable))
+    numlevels = len(levels)
+    # calculate the grand means
+    variable1mean = np.mean(variable1)
+    variable2mean = np.mean(variable2)
+    # calculate the between-groups covariance
+    Covb = 0.0
+    for leveli in levels:
+        levelidata1 = variable1[groupvariable==leveli]
+        levelidata2 = variable2[groupvariable==leveli]
+        mean1 = np.mean(levelidata1)
+        mean2 = np.mean(levelidata2)
+        levelilength = len(levelidata1)
+        term1 = (mean1 - variable1mean) * (mean2 - variable2mean) * levelilength
+        Covb += term1
+    Covb /= numlevels - 1
+    return Covb
+
+# correlation for multivariate data
+corr = stats.pearsonr(X.V2, X.V3)
+print("p-value:\t", corr[1])
+print("cor:\t\t", corr[0])
+corrmat = X.corr() # creates a correlation Matrix
+corrmat
+sns.heatmap(corrmat, vmax=1., square=False).xaxis.tick_top() # correlation heatmap
+
+def mosthighlycorrelated(mydataframe, numtoreport):
+    # find the correlations
+    cormatrix = mydataframe.corr()
+    # set the correlations on the diagonal or lower triangle to zero,
+    # so they will not be reported as the highest ones:
+    cormatrix *= np.tri(*cormatrix.values.shape, k=-1).T
+    # find the top n correlations
+    cormatrix = cormatrix.stack()
+    cormatrix = cormatrix.reindex(cormatrix.abs().sort_values(ascending=False).index).reset_index()
+    # assign human-friendly names
+    cormatrix.columns = ["FirstVariable", "SecondVariable", "Correlation"]
+    return cormatrix.head(numtoreport)
+
+# standardise variables if they show a wide range of statistics
+standardisedX = scale(X)
+standardisedX = pd.DataFrame(standardisedX, index=X.index, columns=X.columns)
+standardisedX.apply(np.mean)
+standardisedX.apply(np.std)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Kanger_soil
+First attempt to use GitHub as a database for all scripts, notes and files I am using to create several geographical maps in the Kangerlussuaq area in South West Greenland. These maps will form the basis for one of the objectives of my PhD project.
+
+I don't have any experience with coding and am here to learn and explore all that I can do with my data to make it even more valuable. 
